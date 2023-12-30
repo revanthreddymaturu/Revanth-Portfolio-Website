cb198ffbc751d083034187d644ed68add75c7428
Revanth Reddy Maturu <mrevanth1999@gmail.com>
2023-12-30 13:15:14 -0500

    Updated Portfolio

---
+++ script.js
@@ -0,0 +1,31 @@
+// const x=document.getElementsByClassName("bgChange")
+
+// function handler(){
+//     const a=document.getElementsByTagName("body");
+//     const b=document.getElementsByTagName("header");
+//     const c=document.getElementsByClassName("my--xp__item");
+//     const d=document.getElementsByClassName("my--xp__item");
+//     const e=document.getElementsByClassName("skills");
+//     const f=document.getElementsByClassName("my__projects");
+//     const g=document.getElementsByClassName("contact__me");
+//     if(a[0]?.classList.contains("bg-change-white")){
+//         a[0]?.classList.remove("bg-change-white");
+//         b[0]?.classList.remove("bg-change-white");
+//         c[0]?.classList.remove("bg-change-white");
+//         d[0]?.classList.remove("bg-change-white");
+//         e[0]?.classList.remove("bg-change-white");
+//         f[0]?.classList.remove("bg-change-white");
+//         g[0]?.classList.remove("bg-change-white");
+
+//     }
+//     else{
+//     a[0]?.classList.add("bg-change-white");
+//     b[0]?.classList.add("bg-change-white");
+//     c[0]?.classList.add("bg-change-white");
+//     d[0]?.classList.add("bg-change-white");
+//     e[0]?.classList.add("bg-change-white");
+//     f[0]?.classList.add("bg-change-white");
+//     g[0]?.classList.add("bg-change-white");
+//     }
+// }
+// x[0]?.addEventListener("click",handler);
